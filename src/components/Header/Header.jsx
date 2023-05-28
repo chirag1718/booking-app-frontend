ce91cf83bcb46a0df57959efc30a5ca24ccf48b4
Chirag <chiragsonar1718@gmail.com>
2023-05-28 21:45:52 +0530

    Feat: Added UI elements to Header - Title, Description, Signin/Register and Searchbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,63 @@
 import "./header.css";
 import HotelIcon from "@mui/icons-material/Hotel";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import LocalTaxiIcon from "@mui/icons-material/LocalTaxi";
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import PersonIcon from "@mui/icons-material/Person";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-list">
-        <div className="headerListItem">
-          <HotelIcon />
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <HotelIcon />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <AirplanemodeActiveIcon />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem ">
+            <DirectionsCarIcon />
+            <span>Car Rental</span>
+          </div>
+          <div className="headerListItem">
+            <HotelIcon />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <LocalTaxiIcon />
+            <span>Aiport Taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free Tripsy acoount
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        {/* Search bar 👇🏻 */}
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <HotelIcon className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <CalendarMonthIcon className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <PersonIcon className="headerIcon" />
+            <span className="headerSearchText">2 Adults 2 Childern 1 Room</span>
+          </div>
+          <div className="headerSearchItem">
+          <button className="headerBtn">Search</button>
+          </div>
         </div>
       </div>
     </div>
