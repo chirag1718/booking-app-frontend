92940d2669acf7690302a06be08bd252a618085a
Chirag <chiragsonar1718@gmail.com>
2023-05-28 11:44:37 +0530

    Feat: Created Home, Hotel and List page and added Navbar and Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./header.css";
+import HotelIcon from "@mui/icons-material/Hotel";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-list">
+        <div className="headerListItem">
+          <HotelIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
