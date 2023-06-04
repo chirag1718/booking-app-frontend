0ab595e660a762172d3d72ed736466ed0d8e267b
Chirag <chiragsonar1718@gmail.com>
2023-06-04 12:10:11 +0530

    feat: added search in List page which will render the selected date, rooms, and destination.

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import "./list.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header";
+import { useLocation } from "react-router-dom";
 
 const List = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" />
+            </div>
+            <div className="lsItem">
+              <label>Check-In Date</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
