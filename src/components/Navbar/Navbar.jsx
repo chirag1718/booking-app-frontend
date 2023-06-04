17289c907825136cbdc31e5f07465c4541a34ed3
Chirag <chiragsonar1718@gmail.com>
2023-06-04 12:07:56 +0530

    added navigate fucn to navigate to home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate("/");
+  };
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Tripsy Booking</span>
+        <span className="logo" onClick={handleHome}>
+          Tripsy Booking
+        </span>
         <div className="navItems">
           <button className="navButton">Resgiter</button>
           <button className="navButton ">Login</button>
