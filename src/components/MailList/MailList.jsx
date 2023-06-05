a896f5612dd65edb11060dac9cc21271deea1074
Chirag <chiragsonar1718@gmail.com>
2023-06-05 10:00:21 +0530

    Fix: fixed width error.

--- src/components/MailList/MailList.jsx
+++ src/components/MailList/MailList.jsx
@@ -3,13 +3,15 @@ import "./mailList.css";
 const MailList = () => {
   return (
     <div className="mail">
-      <h1 className="mailTitle">Save Time Save Money</h1>
-      <span className="mailDesc">
-        Sign up and we'll send the best deals to you
-      </span>
-      <div className="mailInputContainer">
-        <input type="text" placeholder="Your E-mail" />
-        <button>Subscribe</button>
+      <div className="mailWrapper">
+        <h1 className="mailTitle">Save Time Save Money</h1>
+        <span className="mailDesc">
+          Sign up and we'll send the best deals to you
+        </span>
+        <div className="mailInputContainer">
+          <input type="text" placeholder="Your E-mail" />
+          <button>Subscribe</button>
+        </div>
       </div>
     </div>
   );
