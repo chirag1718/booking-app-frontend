49136d2fc759dfda52e3d6972db20bc3f4dd26ce
Chirag <chiragsonar1718@gmail.com>
2023-06-12 12:52:53 +0530

    feat: added authcontextprovider.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import List from "./pages/List/List";
 import Hotel from "./pages/Hotel/Hotel";
-import "./index.css"
+import "./index.css";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
         <Route exact path="/hotels" element={<List />} />
         <Route exact path="/hotels/:id" element={<Hotel />} />
       </Routes>
