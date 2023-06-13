2027c93c5277771871519b1b48f2770394e2dd8e
Chirag <chiragsonar1718@gmail.com>
2023-06-13 10:43:02 +0530

    feat: added feature to reserver hotel and if a room is available or for the selected dates or not

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import useFetch from "../../Hooks/useFetch";
 import "./reservation.css";
+import { SearchContext } from "../../Context/SearchContext";
 import CancelIcon from "@mui/icons-material/Cancel";
+import axios from "axios";
 
 const Reservation = ({ setOpen, hotelId }) => {
   const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
   const [selectedRooms, setSelectedRooms] = useState([]);
+  const { dates } = useContext(SearchContext);
+
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const date = new Date(start.getTime());
+    const dates = [];
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+
+    return dates;
+  };
+
+  const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      allDates.includes(new Date(date).getTime())
+    );
+    return !isFound;
+  };
+
   const handleSelectRoom = (e) => {
     const checked = e.target.checked;
     const value = e.target.value;
@@ -15,12 +41,22 @@ const Reservation = ({ setOpen, hotelId }) => {
         : selectedRooms.filter((item) => item !== value)
     );
   };
-  const handleReserve = () => {
-    
-  }
+
+  const handleReserve = async () => {
+    try {
+      await Promise.all(
+        selectedRooms.map((roomId) => {
+          const res = axios.put(`/rooms/availability/${roomId}`, {
+            date: allDates,
+          });
+          return res.data;
+        })
+      );
+    } catch (err) {}
+  };
   return (
     <div className="reserve">
-      <div className="reserveContainer">
+      <div className="rContainer">
         <CancelIcon
           className="rClose"
           sx={{ fontSize: "30px" }}
@@ -28,7 +64,7 @@ const Reservation = ({ setOpen, hotelId }) => {
         />
         <span>Select your rooms</span>
         {data?.map((item) => (
-          <div className="rItem">
+          <div className="rItem" key={item._id}>
             <div className="rItemInfo">
               <div className="rTitle">{item.title}</div>
               <div className="rDesc">{item.desc}</div>
@@ -37,19 +73,24 @@ const Reservation = ({ setOpen, hotelId }) => {
               </div>
               <div className="rPrice">{item.price}</div>
             </div>
-            {item?.roomNumbers?.map((roomNumber) => (
-              <div className="room">
-                <label>{roomNumber.number}</label>
-                <input
-                  type="checkbox"
-                  value={roomNumber._id}
-                  onChange={handleSelectRoom}
-                />
-              </div>
-            ))}
+            <div className="rSelectRooms">
+              {item?.roomNumbers?.map((roomNumber) => (
+                <div className="room" key={roomNumber.id}>
+                  <label>{roomNumber.number}</label>
+                  <input
+                    type="checkbox"
+                    value={roomNumber.id}
+                    onChange={handleSelectRoom}
+                    disabled={!isAvailable(roomNumber)}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         ))}
-        <button onClick={handleReserve} className="rButton">Reserve Now</button>
+        <button onClick={handleReserve} className="rButton">
+          Reserve Now
+        </button>
       </div>
     </div>
   );
